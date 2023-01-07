f3afd3a14a06fed2190604b7c6a30cddd6e5e42d
Sara <sara.s.q@hotmail.com>
2023-01-07 13:37:58 +0100

    update Home.jsx

---
+++ src/containers/Home/Home.jsx
@@ -0,0 +1,62 @@
+import React, {useEffect, useState} from 'react'
+import { FilmRentalService } from '../../services/FilmRentalService'
+import { useNavigate } from 'react-router-dom'
+import "./Home.scss"
+import Movie from '../../components/Movie/Movie'
+import MoviePages from '../../components/MoviePages/MoviePages'
+
+
+export function Home(){
+    //hooks
+    const [movies, setMovies] = useState([]);
+    const [page, setPage] = useState(1);
+    const [pages, setPages] = useState(1);
+
+
+    useEffect(()=>{
+        getPopularMovies(pages)
+    }, [page])
+
+    
+    //function
+    const getPopularMovies = async (page) =>{
+        try{
+            const res = await FilmRentalService.getPopularMovies(page)
+            setMovies(res.data.results)
+            setPages(res.data.page)
+            
+        } catch (error){
+            console.log(error.message || error);
+        }
+    };
+
+    return (
+        <div className= "container">
+            <div className="background">
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+   <span></span>
+</div>
+        <h1 className="h1 mt-5 mb-5 pb-5">Movie LIST</h1>
+        <MoviePages page={page} pages={pages} setPage={setPage}/>
+        <div className="movies-list">
+        {movies.length>0 &&
+        movies.map((mov)=> (<Movie key={mov.id} movie={mov}/>))}
+        </div>
+        <MoviePages/>
+    </div>
+    )
+
+}
