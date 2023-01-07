aa7152fb920c7eb66a8dc7f9b39b246b8e38adad
Sara <sara.s.q@hotmail.com>
2023-01-07 13:28:49 +0100

    added header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.scss";
+import React from 'react'
+import NavBar from "../NavBar/NavBar"
+
+
+export function Header(){
+    return (
+        <div className="Header">
+            <NavBar/>
+        </div>
+    );
+}
